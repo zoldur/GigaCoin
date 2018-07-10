5cc5d55ab7a1263c860d894e11970530f95fc077
Zoldur <zoldur99@gmail.com>
2018-07-10 18:22:37 +0100

    First release

--- giga_install.sh
+++ giga_install.sh
@@ -25,9 +25,7 @@ function download_node() {
   cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
   compile_error
-  unzip $COIN_ZIP
-  chmod +x $COIN_DAEMON $COIN_CLI
-  cp $COIN_DAEMON $COIN_CLI $COIN_PATH
+  tar xvzf $COIN_ZIP -C $COIN_PATH
   cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
